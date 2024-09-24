51949e4bd7387508ed9c7b77a6f981eface21619
debhere <debmalya.mondal@outlook.com>
2024-09-24 12:00:43 +0530

    test file

--- test.py
+++ test.py
@@ -1,12 +1,17 @@
-def test(x: int):
-    num_type = None
-    if x % 2 == 0:
-        num_type = "even"
-    else:
-        num_type = "odd"
-    return num_type
+import http.client
+import requests
+import pandas as pd
 
+def test():
+    url = "https://api.exchange.coinbase.com/products/BTC-USD/candles?granularity=3600&start=20240101&end=20240102"
+    raw = requests.get(url)
+    candles = raw.json()
+    df = pd.DataFrame(candles, columns = ['tiemstamp', 'low', 'high', 'open', 'close', 'volume'])
+    df.to_csv("btc.csv")
+    #pd.to_csv("cryptodata.csv", df)
+    return df 
 
 if __name__ == "__main__":
-    print(test(5))
+    # print(test())
+    print(test())
     
