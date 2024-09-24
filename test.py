8270c855a6be740fc630b5350982ac0a388c3d4d
debhere <debmalya.mondal@outlook.com>
2024-09-24 20:22:26 +0530

    commit test

--- test.py
+++ test.py
@@ -1,15 +1,19 @@
-import http.client
 import requests
 import pandas as pd
+from datetime import datetime
+
 
 def test():
     url = "https://api.exchange.coinbase.com/products/BTC-USD/candles?granularity=3600&start=20240101&end=20240102"
     raw = requests.get(url)
     candles = raw.json()
-    df = pd.DataFrame(candles, columns = ['tiemstamp', 'low', 'high', 'open', 'close', 'volume'])
-    df.to_csv("btc.csv")
-    #pd.to_csv("cryptodata.csv", df)
-    return df 
+    #df = pd.DataFrame(candles, columns = ['timestamp', 'low', 'high', 'open', 'close', 'volume'])
+    #df.to_csv("btc.csv")
+    #return df 
+    # timestamps = [candle[0] for candle in candles]
+    # datetimes = [datetime.fromtimestamp(timestamp).strftime("%Y-%m-%d %H:%M") for timestamp in timestamps]
+    # return datetimes
+    return candles
 
 if __name__ == "__main__":
     # print(test())
