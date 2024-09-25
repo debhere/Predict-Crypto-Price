213328b049bd49d7d1e71d9e495a4e8e01803803
debhere <debmalya.mondal@outlook.com>
2024-09-25 23:05:31 +0530

    customexp, logger

---
+++ predictcrypto/exception.py
@@ -0,0 +1,19 @@
+import sys
+from .logger import logging
+
+def exception_details(error:Exception, trace:sys) -> str:
+    _,_, exc = trace.exc_info()
+    filename = exc.tb_frame.f_code.co_filename
+
+    error_details = f"Error occured in {filename} in line no. {exc.tb_lineno} : {str(error)}"
+    
+    logging.info(error_details)
+    return error_details
+
+class CustomException(Exception):
+    def __init__(self, error: Exception, trace: sys):
+        super.__init__(error)
+        self.trace = exception_details(error, trace)
+
+    def __str__(self):
+        return self.trace
