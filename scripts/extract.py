6fcff9cec48cc7c4abd963f177fb968b6df80e7f
debhere <debmalya.mondal@outlook.com>
2024-09-28 00:27:24 +0530

    extract and transform

--- scripts/extract.py
+++ scripts/extract.py
@@ -1,42 +1,56 @@
-import sys
-sys.path.append('../')
-
+import os
 import requests
-from datetime import datetime
+from datetime import datetime, timedelta
 import pandas as pd
-from pathlib import Path
 from typing import Optional
-from ..predictcrypto.logger import logging
-from ..predictcrypto.exception import CustomException
+from config import get_etl_config
 
+
+#BTC-USD: Bitcoin, ETH-USD: Ethereum, DOGE-USD: Dogecoin, LTC-USD:Litcoin, USDT-USD: Tether
 def get_ohlc_data_from_coinbase(
         product: Optional[str] = "BTC-USD",
-        start: Optional[str] = "20240101",
-        days: Optional[int] = 180
-) :#-> Path:
-    try:
-        logging.info("Initiating download...")
-        end = datetime.strptime(start, "%Y%m%d") + days
-        print(end)
-    except Exception as e:
-        raise CustomException(e, sys)
-
-
-def pull_data(
-        product: Optional[str] = "BTC-USD", 
-        start: Optional[str] = "20240101", 
-        end: Optional[str] = "20240102"
-): #-> pd.DataFrame:
+        start_date: Optional[str] = "20240101",
+        end_date: Optional[int] = "20240630"
+) -> None:
+        
+    # end_dt = datetime.strptime(start, '%Y%m%d') + timedelta(days=days)
+    # end = end_dt.strftime('%Y%m%d')
+    #print(end_dt, type(end_dt))
     
-    url = f"https://api.exchange.coinbase.com/products/{product}/candles?granularity=3600&start={start}&end={end}"
+    dates = pd.date_range(start = start_date, end = end_date)
+    dates = [day.strftime("%Y%m%d") for day in dates]
+
+    raw_path, interim_path, _ = get_etl_config()
+    
+    next_day = dates[1]
+    for i, day in enumerate(dates):
+        if day == dates[-1]:
+            break
+        next_day = dates[i + 1]
+        df = get_ohlc_data_for_one_day(product, day, next_day)
+        
+        df.to_csv(raw_path, index=False)
+
+        if not interim_path.is_file():
+            interim = pd.DataFrame(list(), df.columns)
+            interim.to_csv(interim_path, index=False)
+        else:
+            interim = pd.read_csv(interim_path)
+
+        interim = pd.concat([df, interim], axis = 0)
+        interim.to_csv(interim_path, index=False)
+    
+    # df_ = pd.read_csv(interim_path)
+    # print(df_.head(10))
+    print(interim.head())
+
+def get_ohlc_data_for_one_day(product, fromDate, toDate) -> pd.DataFrame:
+    url = f"https://api.exchange.coinbase.com/products/{product}/candles?granularity=3600&start={fromDate}&end={toDate}"
     btc = requests.get(url)
     raw = btc.json()
-    #df = pd.DataFrame(raw, columns = ["timestamp", "low", "high", "open", "close", "volume"])
-    # df.to_csv("btc.csv")
-    # return df
-    #print(raw)
-    return raw
+    df = pd.DataFrame(raw, columns = ["ticktimestamp", "lowprice", "highprice", "openprice", "closeprice", "volume"])
+    return df
 
 if __name__ == "__main__":
-    pull_data()
+    #pull_data()
     get_ohlc_data_from_coinbase()
--- scripts/transform.py
+++ scripts/transform.py
@@ -1,15 +1,20 @@
 import pandas as pd
-from extract import pull_data
 from datetime import datetime
+from config import get_etl_config
 
-def transform():
-    batches = pull_data()
-    df = pd.DataFrame(batches, columns = ["ticktimestamp", "lowprice", "highprice", "openprice", "closeprice", "volume"])
-    timestamps = [batch[0] for batch in batches]
-    tickdatetime = [datetime.fromtimestamp(timestamp).strftime('%Y%m%d %H:%M') for timestamp in timestamps]
-    df["tickdatetime"] = tickdatetime
-    #print(df)
-    return df
 
+def transform_ohlc_data(product:str = "BTC-USD") -> None:
+    _, interim_path, trans_path = get_etl_config()
+    trans = pd.read_csv(interim_path)
+    idx = trans[trans["ticktimestamp"].isna()].index
+    trans.drop(idx, inplace=True)
+    
+    trans["ticktimestamp"] = trans["ticktimestamp"].astype("int64")
+    trans["TickDatetime"] = trans["ticktimestamp"].apply(lambda x: datetime.fromtimestamp(x))
+    trans["product"] = product
+
+    trans.to_csv(trans_path, index=False)
+    
 if __name__ == "__main__":
-    transform()
+    #transform()
+    transform_ohlc_data()
