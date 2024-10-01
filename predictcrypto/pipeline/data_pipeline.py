67fe141b1ac246c86b18616d523f018091f224f9
debhere <debmalya.mondal@outlook.com>
2024-10-01 22:36:14 +0530

    updated data pipeline

--- predictcrypto/pipeline/data_pipeline.py
+++ predictcrypto/pipeline/data_pipeline.py
@@ -1,36 +1,29 @@
 import sys
 import pandas as pd
-from dataclasses import dataclass
-from predictcrypto.utils.configurations import fetch_database_configurations
+
+from predictcrypto.utils.datasource import ConnectionManager
 from predictcrypto.logger import logging
 from predictcrypto.exception import CustomException
-from sqlalchemy import create_engine
 
-@dataclass
-class DataPipelineConfig:
-    server, db, driver = fetch_database_configurations()  
+
 
 class DataPipeline:
     def __init__(self):
-        self.configs = DataPipelineConfig()
-    def initiate_data_collection(self):
+        self.cmg = ConnectionManager()
+
+    def build_pipeline(self) -> pd.DataFrame:
         try:
-            db = self.configs.db
-            server = self.configs.server
-            driver = self.configs.driver
+            
+            logging.info("Building the data pipeline...")
 
-            logging.info("Retrieving database connection details")
-
-            conn_string = f'mssql://@{server}/{db}?driver={driver}'
-            engine = create_engine(conn_string)
-
-            logging.info("Establishing database connection")
+            engine = self.cmg.establish_connection()
 
             with engine.connect() as conn, conn.begin():
-                data = pd.read_sql_table("staging.coinbasebtcpricerawdata", conn)
-                logging.info("Connection successful...!!!")
+                logging.info("Connection established...!!")
+                raw = pd.read_sql_table(table_name="coinbasebtcpricerawdata", con=conn, schema="staging")
 
-            return data
+            logging.info("Data is retrieved from the source DB...!!")
+            return raw
         
         except Exception as e:
             raise CustomException(e, sys)
