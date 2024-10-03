af025cede3856777ce64c8d0ee75bbe7e3dd355c
debhere <debmalya.mondal@outlook.com>
2024-10-03 20:00:18 +0530

    updated pipeline module

---
+++ predictcrypto/component/train.py
@@ -0,0 +1,18 @@
+import sys
+import xgboost as xgb
+
+from predictcrypto.exception import CustomException
+from predictcrypto.logger import logging
+
+
+class BaseModel:
+    def __init__(self):
+        pass
+
+
+class BestModel(BaseModel):
+    def __init__(self):
+        pass
+    
+    def data_ingestion(self, X, y):
+        pass
---
+++ predictcrypto/pipeline/transformation_pipeline.py
@@ -0,0 +1,28 @@
+import sys
+import pandas as pd
+from predictcrypto.exception import CustomException
+from predictcrypto.logger import logging
+
+
+class TransformationPipeline:
+    
+    def make_transformation(self, df) -> pd.DataFrame:
+        try:
+            logging.info("Inside Transformation Pipeline")
+            #df = self.dp.build_pipeline()
+            df["tot"] = df["lowprice"] + df["highprice"] + df["openprice"] + df["closeprice"]
+            df["twap"] = df["tot"] / 4
+
+            df["trademonth"] = df["tickdatetime"].dt.month
+            df["tradeday"] = df["tickdatetime"].dt.day
+            df["tradehour"] = df["tickdatetime"].dt.hour
+            df["trademin"] = df["tickdatetime"].dt.minute
+
+            df.index = df["tickdatetime"]
+
+            df_ = df.drop(columns=["tot", "lowprice", "highprice", "openprice", "closeprice", "ticktimestamp", "product", "tickdatetime"])
+
+            logging.info("Transformation complete for training data.")
+            return df_
+        except Exception as e:
+            raise CustomException(e, sys)
