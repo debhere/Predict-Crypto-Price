1550e4c2293c67765845ab3d7657e3b312d497eb
debhere <debmalya.mondal@outlook.com>
2024-10-05 14:38:57 +0530

    structure change

--- setup.py
+++ setup.py
@@ -1,24 +1,24 @@
-from typing import List
-from pathlib import Path
-from setuptools import setup, find_packages
+# from typing import List
+# from pathlib import Path
+# from setuptools import setup, find_packages
 
-def get_requirements(filename: Path) -> List:
+# def get_requirements(filename: Path) -> List:
     
-    with open(filename) as f:
-        requirements = f.readlines()
-        requirements = [r.replace('\n', '') for r in requirements]
-        requirements = [r.replace('-e .', '') for r in requirements]
+#     with open(filename) as f:
+#         requirements = f.readlines()
+#         requirements = [r.replace('\n', '') for r in requirements]
+#         requirements = [r.replace('-e .', '') for r in requirements]
 
-    return requirements
+#     return requirements
 
 
-setup(
-    name = "predictcrypto",
-    version = "0.0.1",
-    author = "Debmalya (Deb)",
-    packages = find_packages(),
-    install_requires = get_requirements("requirements.txt")
-)
+# setup(
+#     name = "predictcrypto",
+#     version = "0.0.1",
+#     author = "Debmalya (Deb)",
+#     packages = find_packages(),
+#     install_requires = get_requirements("requirements.txt")
+# )
 
 
 
