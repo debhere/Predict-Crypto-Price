7598c96bdf90f3a2d473a387e2c70e0e54128a7a
debhere <debmalya.mondal@outlook.com>
2024-10-05 18:58:20 +0530

    updated pipelines

--- predictcrypto/pipeline/transformation_pipeline.py
+++ predictcrypto/pipeline/transformation_pipeline.py
@@ -1,7 +1,7 @@
 import sys
 import pandas as pd
-from src.predictcrypto.exception import CustomException
-from src.predictcrypto.logger import logging
+from predictcrypto.exception import CustomException
+from predictcrypto.logger import logging
 
 
 class TransformationPipeline:
@@ -13,16 +13,17 @@ class TransformationPipeline:
             df["tot"] = df["lowprice"] + df["highprice"] + df["openprice"] + df["closeprice"]
             df["twap"] = df["tot"] / 4
 
-            df["trademonth"] = df["tickdatetime"].dt.month
-            df["tradeday"] = df["tickdatetime"].dt.day
-            df["tradehour"] = df["tickdatetime"].dt.hour
-            df["trademin"] = df["tickdatetime"].dt.minute
+            # df["trademonth"] = df["tickdatetime"].dt.month
+            # df["tradeday"] = df["tickdatetime"].dt.day
+            # df["tradehour"] = df["tickdatetime"].dt.hour
+            # df["trademin"] = df["tickdatetime"].dt.minute
 
-            df.index = df["tickdatetime"]
+            #df.index = df["tickdatetime"]
 
-            df_ = df.drop(columns=["tot", "lowprice", "highprice", "openprice", "closeprice", "ticktimestamp", "product", "tickdatetime"])
+            df_ = df.drop(columns=["tot", "lowprice", "highprice", "openprice", "closeprice", "ticktimestamp", "product", "volume"])
 
             logging.info("Transformation complete for training data.")
+            df_.columns = ["ds", "y"]
             return df_
         except Exception as e:
             raise CustomException(e, sys)
