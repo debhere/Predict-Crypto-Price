c9692da8a2da9f95ba29856522d25736add946c4
debhere <debmalya.mondal@outlook.com>
2024-10-07 14:07:43 +0530

    minor fix in model trainer

--- trainer.py
+++ trainer.py
@@ -1,12 +1,8 @@
 import sys
 #import requests
 import pandas as pd
-from datetime import datetime
-from predictcrypto.exception import CustomException
-from predictcrypto.pipeline.data_pipeline import DataPipeline
-from predictcrypto.pipeline.transformation_pipeline import TransformationPipeline
 
-from predictcrypto.component.train import BestModel
+
 from predictcrypto.component.cmg import training_manager
 
 if __name__ == "__main__":
