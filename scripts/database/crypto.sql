40475e8f4bef62aa4ba0d091846c152fe71c4c85
debhere <debmalya.mondal@outlook.com>
2024-10-15 09:02:38 +0530

    update db script

--- scripts/database/crypto.sql
+++ scripts/database/crypto.sql
@@ -7,8 +7,8 @@ CREATE SCHEMA staging
 GO
 
 
-DROP TABLE IF EXISTS staging.coinbasebtcpricerawdata
-CREATE TABLE staging.coinbasebtcpricerawdata(
+DROP TABLE IF EXISTS staging.coinbasebtcpriceraw
+CREATE TABLE staging.coinbasebtcpriceraw(
 	ticktimestamp BigInt,
 	lowprice decimal(10, 2),
 	highprice decimal(10, 2),
@@ -18,3 +18,38 @@ CREATE TABLE staging.coinbasebtcpricerawdata(
 	tickdatetime datetime,
 	product nvarchar(10)
 )
+
+DROP TABLE IF EXISTS staging.coinbasebtcpricerawhistory
+CREATE TABLE staging.coinbasebtcpricerawhisdata(
+	ticktimestamp BigInt,
+	lowprice decimal(10, 2),
+	highprice decimal(10, 2),
+	openprice decimal(10, 2),
+	closeprice decimal(10, 2),
+	volume decimal(10, 2),
+	tickdatetime datetime,
+	product nvarchar(10)
+)
+
+DROP TABLE IF EXISTS staging.coinbasethpriceraw
+CREATE TABLE staging.coinbaseethpriceraw(
+	ticktimestamp BigInt,
+	lowprice decimal(10, 2),
+	highprice decimal(10, 2),
+	openprice decimal(10, 2),
+	closeprice decimal(10, 2),
+	volume decimal(10, 2),
+	tickdatetime datetime,
+	product nvarchar(10)
+)
+
+DROP TABLE IF EXISTS staging.coinbasethcpricerawhistory
+CREATE TABLE staging.coinbasethpricerawhisdata(
+	ticktimestamp BigInt,
+	lowprice decimal(10, 2),
+	highprice decimal(10, 2),
+	openprice decimal(10, 2),
+	closeprice decimal(10, 2),
+	volume decimal(10, 2),
+	tickdatetime datetime,
+	product nvarchar(10)
