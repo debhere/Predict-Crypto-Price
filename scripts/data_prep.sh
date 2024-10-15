9223bbb2c21fd8d956d57333638f038f898611a8
debhere <debmalya.mondal@outlook.com>
2024-10-15 09:06:54 +0530

    minor change in data prep script

--- scripts/data_prep.sh
+++ scripts/data_prep.sh
@@ -4,11 +4,14 @@ rm -rf downloads
 
 echo "Downloading data...!!!"
 
-product="BTC-USD"
+#products=("BTC-USD", "ETH-USD", "DOGE-USD", "LTC-USD", "USDT-USD")
+#product="BTC-USD"
 start_date="20240101"
-end_date="20241006"
+end_date="20241014"
 
-python scripts/data_loader.py $product $start_date $end_date
+#python scripts/data_loader.py $product $start_date $end_date
+
+python scripts/data_loader.py $start_date $end_date
 
 sleep 2
 
